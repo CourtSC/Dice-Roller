e66ce0c0c95d2bbcbecb50196c03355cde0fd6da
Court <SCO941@multihosp.net>
2022-05-27 15:28:30 -0400

    Added ability to roll multiple dice separated by a comma

--- diceRoller.py
+++ diceRoller.py
@@ -18,7 +18,6 @@ while True: # Main program loop
             sys.exit()
             
         # TODO: Add a help dialogue.
-        # TODO: Allow rolling multiple different dice at once. Ex: 1d4 + 1d6
 
         # Clean up input.
         diceStr = diceStr.lower().replace(' ','')
@@ -29,64 +28,69 @@ while True: # Main program loop
         disIndex = diceStr.find('dis')
         diceStr = diceStr.replace('dis','')
 
-        # Find the index of "d" in the input.
-        dIndex = diceStr.find('d') # The index of the "d" character.
-        if dIndex == -1:
-            raise Exception ('Missing the "d" character.')
+        # TODO: Allow rolling multiple different dice at once. Ex: 1d4, 1d6
+        diceStr = diceStr.split(',')
+
+        for roll in diceStr:
         
-        # Find the number of dice.
-        numberOfDice = int(diceStr[:dIndex])
-
-        # Find the index of the modifier if one exists. Find the modifier.
-        if '+' in diceStr: # There is a '+' modifier.
-            modSignIndex = diceStr.find('+')
-            modSign = '+'
-            diceMod = int(diceStr[modSignIndex + 1:])
-        elif '-' in diceStr: # There is a '-' modifier.
-            modSignIndex = diceStr.find('-') 
-            modSign = '-'
-            diceMod = -int(diceStr[modSignIndex + 1:])
-        else: # There is no modifier.
-            modSignIndex = -1 
-            modSign = '+'
-            diceMod = 0
-
-        # Find the size of the dice.
-        if modSignIndex == -1: # There is no modifier to the roll.
-            sizeOfDice = int(diceStr[dIndex + 1:])
-        else:
-            sizeOfDice = int(diceStr[dIndex + 1: modSignIndex])
-
-        # Display the total.
-        if advIndex != -1 or disIndex != -1:
-            roll1 = diceRolls()
-            roll2 = diceRolls()
-
-        if advIndex != -1: # Roll with Advantage.
-            rolls = max(roll1,roll2)
-            print(f'Total: {sum(rolls) + diceMod}')
-            advRolls = '\u0336'+'\u0336'.join(str(min(roll1, roll2)))
-            print(f'{rolls}, {advRolls}')
-
-        elif disIndex != -1: # Roll with Disadvantage.
-            rolls = min(roll1, roll2)
-            print(f'Total: {sum(rolls) + diceMod}')
-            disRolls = '\u0336'+'\u0336'.join(str(max(roll1, roll2)))
-            print(f'{rolls}, {disRolls}')
-
-        else: # Roll normally.
-            rolls = diceRolls()
-            print(f'Total: {sum(rolls) + diceMod}')
-            print(f'Rolls: {rolls}')
+            # Find the index of "d" in the input.
+            dIndex = roll.find('d') # The index of the "d" character.
+            if dIndex == -1:
+                raise Exception ('Missing the "d" character.')
             
-        if diceMod != 0:
-            print(f'Modifier: {modSign} {abs(diceMod)}')
-        print('\n')
+            # Find the number of dice.
+            numberOfDice = int(roll[:dIndex])
 
-        # Verbose testing.
-        # print(f'modSignIndex Value: {modSignIndex}')
-        # print(f'modSign Value: {modSign}')
-        # print(f'diceMod Value: {diceMod}')
+            # Find the index of the modifier if one exists. Find the modifier.
+            if '+' in roll: # There is a '+' modifier.
+                modSignIndex = roll.find('+')
+                modSign = '+'
+                diceMod = int(roll[modSignIndex + 1:])
+            elif '-' in roll: # There is a '-' modifier.
+                modSignIndex = roll.find('-') 
+                modSign = '-'
+                diceMod = -int(roll[modSignIndex + 1:])
+            else: # There is no modifier.
+                modSignIndex = -1 
+                modSign = '+'
+                diceMod = 0
+
+            # Find the size of the dice.
+            if modSignIndex == -1: # There is no modifier to the roll.
+                sizeOfDice = int(roll[dIndex + 1:])
+            else:
+                sizeOfDice = int(roll[dIndex + 1: modSignIndex])
+
+            # Display the total.
+            if advIndex != -1 or disIndex != -1:
+                roll1 = diceRolls()
+                roll2 = diceRolls()
+
+            if advIndex != -1: # Roll with Advantage.
+                rolls = max(roll1,roll2)
+                print(f'Total: {sum(rolls) + diceMod}')
+                advRolls = '\u0336'+'\u0336'.join(str(min(roll1, roll2)))
+                print(f'{rolls}, {advRolls}')
+
+            elif disIndex != -1: # Roll with Disadvantage.
+                rolls = min(roll1, roll2)
+                print(f'Total: {sum(rolls) + diceMod}')
+                disRolls = '\u0336'+'\u0336'.join(str(max(roll1, roll2)))
+                print(f'{rolls}, {disRolls}')
+
+            else: # Roll normally.
+                rolls = diceRolls()
+                print(f'Total: {sum(rolls) + diceMod}')
+                print(f'Rolls: {rolls}')
+                
+            if diceMod != 0:
+                print(f'Modifier: {modSign} {abs(diceMod)}')
+            print('\n')
+
+            # Verbose testing.
+            # print(f'modSignIndex Value: {modSignIndex}')
+            # print(f'modSign Value: {modSign}')
+            # print(f'diceMod Value: {diceMod}')
 
     except Exception as exc:
         # Catch any exceptions and display to user.
